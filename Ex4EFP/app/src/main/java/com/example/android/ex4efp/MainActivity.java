d23f9d3c62eab857ed989f55c2700ff73f01e261
twotubs <32124175+twotubs@users.noreply.github.com>
2018-05-29 13:03:12 +0200

    Add files via upload

---
+++ Ex4EFP/app/src/main/java/com/example/android/ex4efp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.android.ex4efp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitAnswer(View view) {
+
+        //Get user's quote
+        EditText quoteField = (EditText) findViewById(R.id.quote_field);
+        Editable quoteEditable = quoteField.getText();
+        String quote = quoteEditable.toString();
+
+        //Get the person that said the quote
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        Editable nameEditable = nameField.getText();
+        String name = nameEditable.toString();
+
+
+
+
+        TextView textViewToChange = (TextView) findViewById(R.id.quote_answer);
+        textViewToChange.setText("The " + quote + " was " + name + ".");
+
+    }
+}
